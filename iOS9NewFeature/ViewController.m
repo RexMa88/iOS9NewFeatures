263d198206f065a78997eae9073a065bfb97c577
RexMa88 <rex.smith98@gmail.com>
2015-12-15 19:44:10 +0800

    first commit

--- iOS9NewFeature/ViewController.m
+++ iOS9NewFeature/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString *cellIdentifier = @"cell";
+
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonnull, nonatomic, strong) NSArray<NSString *> *titleArray;
+
+@property (nonatomic, strong) UITableView * tableView;
 
 @end
 
@@ -17,6 +23,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.titleArray = @[@"Hello",@"world",@"Rex",@"Ma",@"Shine",@"Zhou"];
+    [self customTableView];
+}
+
+- (void)customTableView{
+    self.tableView = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].bounds style:UITableViewStyleGrouped];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    [self.view addSubview:self.tableView];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.titleArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    cell.textLabel.text = self.titleArray[indexPath.row];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
